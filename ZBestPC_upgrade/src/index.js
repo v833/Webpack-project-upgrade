774b8f4cceff716336f16e811763b90b9d906bc6
v833 <2507301541@qq.com>
2022-04-12 00:28:56 +0800

    chore: 引入devServer

--- ZBestPC_upgrade/src/index.js
+++ ZBestPC_upgrade/src/index.js
@@ -1,4 +1,5 @@
-import './index.css';
+// import 'style-loader!css-loader!./index.css'; // inline style
+import '/index.css'; // inline style
 import value from './index.wq'
 console.log(value);
 console.log('hello world');
--- ZBestPC_upgrade/webpack.config.js
+++ ZBestPC_upgrade/webpack.config.js
@@ -1,24 +1,29 @@
 // 指向当前weppack.config.js文件的相对路径
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  devtool: 'source-map',
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "eval-cheap-module-source-map",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  devServer: {
+    static: "./public",
+    port: "1115",
+    open: true,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader','css-loader'],
+        use: [{ loader: "style-loader" }, { loader: "css-loader" }],
       },
       {
         test: /\.wq$/,
-        use: [path.resolve(__dirname, './loader/wq-loader.js')],
-      }
-    ]
-  }
-}
+        use: [path.resolve(__dirname, "./loader/wq-loader.js")],
+      },
+    ],
+  },
+};
